7115d707f635b4836691168ba6e5ac3f32daa413
stepbirt <stepbirt@gmail.com>
2023-01-01 21:53:07 +0700

    add app listen log

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = 3000;
+  await app.listen(3000, '0.0.0.0');
+
+  Logger.log(`🚀 Application is running on: ${await app.getUrl()}/`);
+  Logger.log(`Environment at ${port}`);
 }
 bootstrap();
